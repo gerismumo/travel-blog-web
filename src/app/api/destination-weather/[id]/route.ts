3bcda1bbf98bb9321d6a0e73faf0d6a9c23e57b0
gerismumo <geraldmumo6@gmail.com>
2024-06-17 09:42:04 +0300

    added api for update weather data

--- src/app/api/destination-weather/[id]/route.ts
+++ src/app/api/destination-weather/[id]/route.ts
@@ -18,3 +18,27 @@ export async function DELETE(req: NextRequest) {
         return NextResponse.json({ success: false, message: 'server error' }); 
     }
 }
+
+export async function PUT(req: NextRequest) {
+    const url = new URL(req.url);
+    //get id
+    const id = url.pathname.split('/').pop();  
+    try {
+        const body = await req.json();
+        for(const[key, value] of Object.entries(body)) {
+            if(!value) {
+               return NextResponse.json({ success:false, message:"all fields are required"});
+            }
+          }
+
+        await connectDB(); 
+        const updateData = await DestinationWeatherData.findByIdAndUpdate(id, body, { new: true });
+        if(updateData) {
+            return NextResponse.json({ success: true, message: 'Updated successfully' });
+        }else {
+            return NextResponse.json({ success: false, message: 'Something went wrong' });
+        }
+    } catch (error) {
+        return NextResponse.json({ success: false, message:'server error' }); 
+    }
+}
--- src/app/dashboard/weather/page.tsx
+++ src/app/dashboard/weather/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { IWeatherDataList } from '@/(types)/type'
+import { IDestinationList, IWeatherData, IWeatherDataList } from '@/(types)/type'
+import { getDestinations } from '@/utils/(apis)/destinationApi'
 import { getWeatherData } from '@/utils/(apis)/weatherApi'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
@@ -9,6 +10,9 @@ import toast from 'react-hot-toast'
 const page = () => {
   const[data, setData] = useState<IWeatherDataList[]>([])
   const[error, setError] = useState<string | null>(null)
+  const[openEdit, setOpenEdit] = useState<Boolean>(false);
+  const[openEditId, setOpenEditId] = useState<string | null>(null);
+  const [destinations, setDestinations] = useState<IDestinationList[]>([]);
 
   const fetchData = async () => {
     try {
@@ -27,6 +31,21 @@ const page = () => {
     toast.error(error);
 }
 
+//destination list
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await getDestinations();
+          setDestinations(data);
+        } catch (error : any) {
+          setError(error.message);
+        }
+      };
+  
+      fetchData();
+    }, [setDestinations, setError]);
+
+//delete data fun
 const HandleDelete = async(id : string) => {
   try {
     const response = await axios.delete(`/api/destination-weather/${id}`)
@@ -40,6 +59,60 @@ const HandleDelete = async(id : string) => {
     return toast.error("network error");
   }
 }
+
+//edit data fun
+  const[currrentData, setCurrentData] = useState<IWeatherDataList | null>(null);
+
+  const HandleEdit = (id : string) => {
+    setOpenEdit(!openEdit)
+    setOpenEditId(id);
+    setCurrentData(data.find(d => d._id ===id) || null);
+  }
+
+ 
+
+  const handleSubmitEdit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(currrentData === null) {
+      toast.error("no available data");
+      return;
+    }
+
+    const weatherData: IWeatherDataList = {
+      _id: currrentData._id,
+      destinationId: currrentData.destinationId,
+      date: currrentData.condition,
+      airTemperature: currrentData.airTemperature,
+      waterTemperature: currrentData.waterTemperature,
+      humidity: currrentData.humidity,
+      condition : currrentData.condition,
+      sunnyHours: currrentData.sunnyHours,
+    };
+
+    //check empty fields
+    for(const[key, value] of Object.entries(weatherData)) {
+      if(!value) {
+        toast.error("all fields are required");
+        return;
+      }
+    }
+
+    //submit data
+    try {
+      const response = await axios.put(`/api/destination-weather/${currrentData._id}`, weatherData);
+      if(response.data.success) {
+        toast.success(response.data.message);
+        setOpenEdit(false);
+        fetchData();
+      }else {
+        toast.error(response.data.message);
+      }
+    }catch(error: any) {
+      return toast.error("network error");
+    }
+    
+  }
 
   return (
     <div className="flex flex-col">
@@ -73,7 +146,9 @@ const HandleDelete = async(id : string) => {
                     <td className='border border-slate-600 px-[20px] py-[15px] '>{d.condition}</td>
                     <td className='border border-slate-600 px-[20px] py-[15px] '>{d.sunnyHours}</td>
                     <td className='border border-slate-600 px-[20px] py-[15px] '>
-                      <button>Edit</button>
+                      <button
+                      onClick={() => HandleEdit(d._id)}
+                      >Edit</button>
                     </td>
                     <td className='border border-slate-600 px-[20px] py-[15px] '>
                       <button
@@ -81,6 +156,124 @@ const HandleDelete = async(id : string) => {
                       >Delete</button>
                     </td>
                   </tr>
+                  {openEdit && openEditId === d._id && (
+                    <tr>
+                      <td className='border border-slate-600 px-[20px] py-[15px]' colSpan={9}>
+                        <div className="flex flex-col">
+                          <form onSubmit={handleSubmitEdit} 
+                            className="flex flex-col gap-[10px] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="destination">
+                                Destination
+                              </label>
+                              <select name="destination" id="destination"
+                              className='input w-full'
+                                value={currrentData?.destinationId}
+                                onChange={(e) => setCurrentData(currrentData ? {...currrentData, destinationId: e.target.value}: null)}
+                              >
+                                <option value="">select destination</option>
+                                {destinations.length > 0 && destinations.map((obj) => (
+                                  <option key={obj._id} value={obj._id}>{obj.name}</option>
+                                ))}
+                              </select>
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="date">
+                                Date
+                              </label>
+                              <input type="date" name="date" id="date"
+                              value={currrentData?.date}
+                              onChange={(e) => setCurrentData(currrentData ? { ...currrentData, date: e.target.value } : null)}
+                              className='input'
+                              />
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold" htmlFor="temperature">
+                                Air Temperature (°C)
+                              </label>
+                              <input
+                                className="input"
+                                id="temperature"
+                                type="number"
+                                placeholder="Enter temperature"
+                                value={currrentData?.airTemperature}
+                                onChange={(e) => setCurrentData(currrentData ? {...currrentData, airTemperature: e.target.value}: null)}
+                              />
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="waterTemperature">
+                              Water Temperature (°C)
+                              </label>
+                              <input
+                                className="input"
+                                id="waterTemperature"
+                                type="number"
+                                placeholder="Enter water temperature"
+                                value={currrentData?.waterTemperature}
+                                onChange={(e) => setCurrentData(currrentData ? {...currrentData, waterTemperature: e.target.value}: null)}
+                              />
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="humidity">
+                                Humidity (%)
+                              </label>
+                              <input
+                                className="input"
+                                id="humidity"
+                                type="number"
+                                placeholder="Enter humidity"
+                                value={currrentData?.humidity}
+                                onChange={(e) => setCurrentData(currrentData ? {...currrentData, humidity: e.target.value}: null)}
+                              />
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="sunnyHours">
+                              Condition
+                              </label>
+                              <select name="condition" id="condition"
+                              value={currrentData?.condition}
+                              onChange={(e) => setCurrentData(currrentData ? {...currrentData, condition: e.target.value}: null)}
+                              className='input'
+                              >
+                                <option value="">select condition</option>
+                                <option value="Sunny">Sunny</option>
+                                <option value="Cloudy">Cloudy</option>
+                                <option value="Rainy">Rainy</option>
+                                <option value="Snowy">Snowy</option>
+                                <option value="Windy">Windy</option>
+                                <option value="Foggy">Foggy</option>
+                                <option value="Dry">Dry</option>
+                                <option value="Hot">Hot</option>
+                                <option value="Cold">Cold</option>
+                                <option value="Windy">Windy</option>
+                              </select>
+                            </div>
+                            <div className="flex flex-col">
+                              <label className="block text-gray-700 text-sm font-bold " htmlFor="sunnyHours">
+                              Sunny Hours
+                              </label>
+                              <input
+                                className="input"
+                                id="sunnyHours"
+                                type="number"
+                                placeholder="sunny hours"
+                                value={currrentData?.sunnyHours}
+                                onChange={(e) => setCurrentData(currrentData ? {...currrentData, sunnyHours: e.target.value}: null)}
+                              />
+                            </div>
+                            <div className="flex flex-row w-[100%]">
+                              <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit"
+                              >
+                                Submit
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                      </td>
+                    </tr>
+                  )} 
                 </React.Fragment>
               ))}
               
