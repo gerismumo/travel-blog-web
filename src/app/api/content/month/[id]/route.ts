38ec066bebdacc12e2433271686c731d83452e9e
gerismumo <geraldmumo6@gmail.com>
2024-06-20 18:06:09 +0300

    changed how to call api

--- src/app/api/content/month/[id]/route.ts
+++ src/app/api/content/month/[id]/route.ts
@@ -1,35 +1,32 @@
 
 import { DestinationMonthContent } from "@/(models)/destination";
 import connectDB from "@/utils/dbConnect";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req:NextRequest) {
-    const url = new URL(req.url);
-    const id = url.pathname.split('/').pop(); 
+
+export async function DELETE(req:Request, {params}: {params: {id: string}}) {
     try {
         await connectDB(); 
-        const deleteData = await DestinationMonthContent.findByIdAndDelete(id);
+        const deleteData = await DestinationMonthContent.findByIdAndDelete(params.id);
         if(deleteData) {
-            return NextResponse.json({success: true, message: "delete success"})
+            return Response.json({success: true, message: "delete success"})
         } else {
-            return NextResponse.json({success: false, message: "delete failed"})
+            return Response.json({success: false, message: "delete failed"})
         }
     } catch (error) {
-        return NextResponse.json({success: false, message: "delete failed"})
+        return Response.json({success: false, message: "delete failed"})
     }
 }
 
 
-export async function PUT(req: NextRequest) {
-    const url = new URL(req.url);
-    const id = url.pathname.split('/').pop(); 
+export async function PUT(req: Request, {params}: {params: {id: string}}) {
+
     try{
         const body = await req.json();
 
         const {destinationId, month, weatherInfo} = body;
 
         if(destinationId === "" || month === "" || weatherInfo === "") {
-            return NextResponse.json({success: false, message: "all fields are required"})
+            return Response.json({success: false, message: "all fields are required"})
         }
         
         await connectDB();
@@ -37,26 +34,26 @@ export async function PUT(req: NextRequest) {
         let updateData;
         const existingRecord = await DestinationMonthContent.findOne({ destinationId, month });
         if(existingRecord) {
-            updateData = await DestinationMonthContent.findByIdAndUpdate(id, 
+            updateData = await DestinationMonthContent.findByIdAndUpdate(params.id, 
                 {
                     weatherInfo: body.weatherInfo,
                 },
                 {new: true}
             )
-            return NextResponse.json({ success: true, message: 'Record with same date updated' });
+            return Response.json({ success: true, message: 'Record with same date updated' });
 
         }else {
-            updateData = await DestinationMonthContent.findByIdAndUpdate(id, {
+            updateData = await DestinationMonthContent.findByIdAndUpdate(params.id, {
                 destinationId, month, weatherInfo
             }, {new: true});
         }
 
         if (updateData) {
-            return NextResponse.json({ success: true, message: 'Updated successfully' });
+            return Response.json({ success: true, message: 'Updated successfully' });
           } else {
-            return NextResponse.json({ success: false, message: 'Something went wrong' });
+            return Response.json({ success: false, message: 'Something went wrong' });
           } 
     }catch(error) {
-        return NextResponse.json({success: false, message: "server error"})
+        return Response.json({success: false, message: "server error"})
     }
 }
