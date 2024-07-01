2be0f676454a6a715b63491b3d9ab56db80f739d
gerismumo <geraldmumo6@gmail.com>
2024-07-01 13:40:48 +0300

    updated the app - page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
-
-export default function() {
+const WeatherDashboard = () => {
   return (
     <div>
       <h1>Weather Dashboard</h1>
     </div>
   );
-}
+};
+
+WeatherDashboard.displayName = "WeatherDashboard";
+
+export default WeatherDashboard;
