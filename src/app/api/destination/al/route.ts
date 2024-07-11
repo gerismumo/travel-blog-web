c8d92cab9a7b00ef62fbf99479b37dbffcdbb1b6
gerismumo <geraldmumo6@gmail.com>
2024-07-11 14:15:36 +0300

    updated the selection of faqs

--- src/app/api/destination/al/route.ts
+++ src/app/api/destination/al/route.ts
@@ -30,7 +30,7 @@ export async function GET(req: Request) {
         let data: any =[]
         await Promise.all(destination.map(async(d) => {
             const content = await DestinationContent.findOne({destination: d._id})
-            const faq = await DestinationFaq.findOne({destination: d._id})
+            const faq = await DestinationFaq.find({destination: d._id})
             const monthContent = await DestinationMonthContent.find({destination: d._id})
             const monthFaq = await DestinationMonthFaq.find({destination: d._id});
             let weatherData = null;
